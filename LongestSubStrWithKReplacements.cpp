5e6c08900c0c7cc3127af9c7c44ceb283271fdc4
Adwitiya Mourya <adwitiya.code@gmail.com>
2026-05-05 21:36:12 +0530

    Better and Optimal Approach - LongestSubStr with K Replacements

--- LongestSubStrWithKReplacements.cpp
+++ LongestSubStrWithKReplacements.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// * TC: O(N²)
 // * BRUTE FORCE :
 class Solution {
 public:
@@ -23,3 +24,57 @@ public:
 
     }
 };
+
+// * BETTER Approach
+// * TC: O(N + N) * 26
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int maxLen = 0, maxFreq = 0, n = s.size();
+        int left = 0, right = 0;
+        int hash[26] = {0};
+        while(right < n){
+            hash[s[right] - 'A']++;
+            maxFreq = max(maxFreq, hash[s[right] - 'A']);
+            while(left < right && ((right - left + 1) - maxFreq) > k) {
+                hash[s[left] - 'A']--;
+                maxFreq = 0;
+                for(int i = 0; i < 26; i++){
+                    maxFreq = max(maxFreq, hash[i]);
+                }
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
+
+    }
+};
+
+
+// * OPTIMAL Approach
+// * TC: O(n) * 26
+int characterReplacement(string s, int k) {
+    int maxLen = 0, maxFreq = 0, n = s.size();
+    int left = 0, right = 0;
+    int hash[26] = {0};
+    while(right < n){
+        hash[s[right] - 'A']++;
+        maxFreq = max(maxFreq, hash[s[right] - 'A']);
+        if(left < right && ((right - left + 1) - maxFreq) > k) {
+            hash[s[left] - 'A']--;
+            maxFreq = 0;
+            for(int i = 0; i < 26; i++){
+                maxFreq = max(maxFreq, hash[i]);
+            }
+            left++;
+        }
+
+        maxLen = max(maxLen, right - left + 1);
+        right++;
+    }
+    return maxLen;
+
+}
