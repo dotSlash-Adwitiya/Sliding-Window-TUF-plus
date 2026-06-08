aef04a43315eaf62215eb671737ffe60bd246b56
Adwitiya Mourya <adwitiya.code@gmail.com>
2026-06-08 20:00:51 +0530

    Most Optimal Approach added - practice

--- LongestSubStrWithKReplacements.cpp
+++ LongestSubStrWithKReplacements.cpp
@@ -78,3 +78,27 @@ int characterReplacement(string s, int k) {
     return maxLen;
 
 }
+
+// * MOST OPTIMAL: O(N)
+int longestSubstr(string& s, int k) {
+    int n = s.size(), maxLen = 0;
+    int l = 0, r = 0;
+    int hash[26] = {0}, maxFreq = 0;
+    while(r < n){
+        hash[s[r] - 'A']++;
+        maxFreq = max(maxFreq, hash[s[r] - 'A']);
+        // * Dont let your window size decrement beyond 1 window pane.
+        if((r - l + 1) - maxFreq > k){
+            hash[s[l] - 'A']--;
+            maxFreq = 0;
+            // for(int i = 0; i < 26; i++)
+            //     maxFreq = max(maxFreq, hash[i]);
+            l++;
+        }
+        // if(changes <= k)
+        maxLen = max(maxLen, r - l + 1);
+        
+        r++;
+    }
+    return maxLen;
+}
