ebbf354cb7cf80f1bcda0f263466af9143841024
Adwitiya Mourya <adwitiya.code@gmail.com>
2026-08-10 21:14:15 +0530

    Fixed Window Problem - BEST PRACTICE CODE

--- PRACTICE/MEDIUM/K-RadiusSubarrAvg.cpp
+++ PRACTICE/MEDIUM/K-RadiusSubarrAvg.cpp
@@ -28,3 +28,29 @@ vector<int> getAverages(vector<int>& nums, int k) {
     }
     return average;
 }
+
+// * Structured, BEST PRACTICES CODE, less no of variables.
+vector<int> getAverages(vector<int>& nums, int k) {
+    int n = nums.size();
+    int windowSize = 2 * k + 1;
+    vector<int> ans(n, -1);
+    if(windowSize > n) return ans;
+    long long windowSum = 0;
+    // * First window: [0 ... 2k]
+    windowSum = accumulate(nums.begin(),nums.begin() + windowSize, 0LL);
+    
+    // * Center of first window
+    int center = k;
+    while(center + k < n) {
+        // * Current window: [center-k ... center+k]
+        ans[center] = windowSum / windowSize;
+
+        // * Don't slide if this was the final window
+        if(center + k + 1 < n) {
+            windowSum -= nums[center - k];
+            windowSum += nums[center + k + 1];
+        }
+        center++;
+    }
+    return ans;
+}
